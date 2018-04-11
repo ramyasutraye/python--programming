6cd3c855ef6bd0dabc7bc5975c498640ffefbb57
vidhyakb <37337231+vidhyakb@users.noreply.github.com>
2018-04-11 18:07:39 +0530

    Create set6-52.py

---
+++ beginner level 1/set6-52.py
@@ -0,0 +1,21 @@
+s=raw_input()
+if (s=="1"):
+	print "One"
+elif (s=="2"):
+	print "Two"
+elif (s=="3"):
+	print "Three"
+elif (s=="4"):
+	print "Four"
+elif (s=="5"):
+	print "Five"
+elif (s=="6"):
+	print "Six"
+elif (s=="7"):
+	print "Seven"
+elif (s=="8"):
+	print "Eight"
+elif (s=="9"):
+	print "Nine"
+else:
+	print "Ten"
